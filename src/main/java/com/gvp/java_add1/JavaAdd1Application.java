03e522cc32cb0d01d936705ea36029ac348aeb9e
SaavithaMaliNandiniAchalla <achallanandini2004@gmail.com>
2024-12-09 22:19:01 +0530

    changes done

---
+++ src/main/java/com/gvp/java_add1/JavaAdd1Application.java
@@ -0,0 +1,13 @@
+package com.gvp.java_add1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvp/java_add1/JavaAdd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvp.java_add1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
